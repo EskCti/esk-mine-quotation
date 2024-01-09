be0bb485a44bc5a4143633341e5d87647cbd3cc4
eskokado <eskokado@gmail.com>
2024-01-09 10:36:01 -0300

    feat: create interface client CurrencyPrice

---
+++ src/main/java/org/eskcti/mine/client/CurrencyPriceClient.java
@@ -0,0 +1,17 @@
+package org.eskcti.mine.client;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.eskcti.mine.dto.CurrencyPriceDTO;
+
+@Path("/last")
+@RegisterRestClient
+@ApplicationScoped
+public interface CurrencyPriceClient {
+    @GET
+    @Path("/{pair}")
+    CurrencyPriceDTO getPriceByPair(@PathParam("pair") String pair);
+}
